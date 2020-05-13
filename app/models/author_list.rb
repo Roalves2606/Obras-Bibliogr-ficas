93a2f1e3aacb45b0d729c16f40643a2bcea6d8c2
Ronaldo Alves <ronaldo@neomed.tech>
2020-05-13 19:34:45 -0300

    Add  more validation methods to author_list model

--- app/models/author_list.rb
+++ app/models/author_list.rb
@@ -1,12 +1,26 @@
 class AuthorList < ApplicationRecord
+  ARRAY_TYPE_ERROR_MESSAGE = 'list must be an array'.freeze
   LENGTH_ERROR_MESSAGE = "length doesn't match specified records".freeze
+  BLANK_RECORDS_ERROR_MESSAGE = "list can't have blank records".freeze
 
   validates :records, :original_list, presence: true
-  validate :original_list_length
+  validate :is_array?, :check_blank_records, :check_array_length
 
   private
 
-  def original_list_length
+  def is_array?
+    return if original_list.is_a?(Array)
+
+    errors.add(:original_list, ARRAY_TYPE_ERROR_MESSAGE)
+  end
+
+  def check_blank_records
+    return unless original_list.try(:any?, &:blank?)
+
+    errors.add(:original_list, BLANK_RECORDS_ERROR_MESSAGE)
+  end
+
+  def check_array_length
     return if original_list.try(:length) == records
 
     errors.add(:original_list, LENGTH_ERROR_MESSAGE)
--- spec/models/author_list_spec.rb
+++ spec/models/author_list_spec.rb
@@ -2,10 +2,75 @@ require 'rails_helper'
 
 RSpec.describe AuthorList do
   describe 'validations' do
+    let(:empty_error) { "can't be blank" }
+    let(:array_type_error) { 'list must be an array' }
+    let(:blank_record_error) { "list can't have blank records" }
+    let(:lenght_error) { "length doesn't match specified records" }
+
     it { is_expected.to validate_presence_of(:records) }
     it { is_expected.to validate_presence_of(:original_list) }
 
-    describe '.original_list_length' do
+    describe '.is_array?' do
+      context 'when original list is an array' do
+        let(:valid_list) { create(:author_list) }
+
+        it 'returns valid object' do
+          expect(valid_list).to be_valid
+        end
+      end
+
+      context "when original list isn't an array" do
+        subject(:invalid_list) { described_class.new(invalid_attributes) }
+
+        let(:invalid_attributes) { { records: 1 } }
+
+        let(:expected_errors) { [empty_error, array_type_error, lenght_error] }
+
+        it 'returns invalid object' do
+          expect(invalid_list).to be_invalid
+        end
+
+        it 'returns correcr error messages' do
+          invalid_list.valid?
+          errors = invalid_list.errors.messages[:original_list]
+
+          expect(errors).to eq(expected_errors)
+        end
+      end
+    end
+
+    describe '.check_blan k_records' do
+      context 'when original list has no blank record' do
+        let(:valid_list) { create(:author_list) }
+
+        it 'returns valid object' do
+          expect(valid_list).to be_valid
+        end
+      end
+
+      context 'when original list has a blank record' do
+        subject(:invalid_list) { described_class.new(invalid_attributes) }
+
+        let(:invalid_attributes) do
+          attributes_for(:author_list, original_list: [''])
+        end
+
+        let(:expected_errors) { [blank_record_error, lenght_error] }
+
+        it 'returns invalid object' do
+          expect(invalid_list).to be_invalid
+        end
+
+        it 'returns correcr error messages' do
+          invalid_list.valid?
+          errors = invalid_list.errors.messages[:original_list]
+
+          expect(errors).to eq(expected_errors)
+        end
+      end
+    end
+
+    describe '.check_list_length' do
       context 'when list lenght matches records' do
         let(:valid_list) { create(:author_list) }
 
@@ -19,19 +84,15 @@ RSpec.describe AuthorList do
 
         let(:invalid_attributes) { attributes_for(:author_list, records: 1) }
 
-        let(:length_error) do
-          { original_list: ["length doesn't match specified records"] }
-        end
-
         it 'returns invalid object' do
           expect(invalid_list).to be_invalid
         end
 
         it 'returns lenth error message object' do
           invalid_list.valid?
-          error_message = invalid_list.errors.messages
+          errors = invalid_list.errors.messages[:original_list]
 
-          expect(error_message).to eq(length_error)
+          expect(errors).to eq([lenght_error])
         end
       end
     end
